dc22b5ace2af0247cf14661c1fa7da00dad13d23
GencoMattia <gencomattia@hotmail.it>
2024-08-21 16:56:25 +0200

    Stylized card

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,7 +1,12 @@
 <script>
 import axios from 'axios';
+import SingleProjectCard from './SingleProjectCard.vue';
 
 export default {
+    components: {
+        SingleProjectCard,
+    }, 
+
     data() {
         return {
             projects: [
@@ -37,49 +42,11 @@ export default {
 <template>
     <div class="container mt-4">
         <div class="row">
-            <div class="col-md-4" v-for="project in projects" :key="project.id">
-                <div class="card project-card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ project.title }}</h5>
-                        <p class="card-text">{{ project.description }}</p>
-                        <a href="#" class="btn btn-primary">Open Project</a>
-                    </div>
-                </div>
-            </div>
+            <SingleProjectCard class="col-md-4" v-for="project in projects" :key="project.id" :project="project"/>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    @use "../assets/styles/partials/variables" as *;
-
-    .project-card {
-        margin-bottom: $card-margin-bottom;
-        box-shadow: $card-box-shadow;
-        transition: transform 0.3s ease-in-out;
-
-        &:hover {
-            transform: $card-hover-transform;
-        }
-
-        .card-title {
-            font-size: $card-title-font-size;
-            font-weight: $card-title-font-weight;
-        }
-
-        .card-text {
-            font-size: $card-text-font-size;
-            color: $card-text-color;
-        }
-
-        .btn-primary {
-            background-color: $btn-primary-bg;
-            border-color: $btn-primary-border;
-
-            &:hover {
-            background-color: $btn-primary-hover-bg;
-            border-color: $btn-primary-hover-border;
-            }
-        }
-    }
+    
 </style>
